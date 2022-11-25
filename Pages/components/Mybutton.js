ce4c408ab72e7bbaf99559cb7dd79601cc81c702
Simplice NIYONZIMA <89002414+Simplice24@users.noreply.github.com>
2022-11-25 09:23:50 -0800

    Mobile Computing Assignment 1 Student Management App in Expo  React-Native-SQLite storage

---
+++ Pages/components/Mybutton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Mybutton = (props) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={props.customClick}>
+      <Text style={styles.text}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    padding: 10,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+    borderRadius:5,
+  },
+  text: {
+    color: 'white',
+    fontWeight:'bold',
+  },
+});
+
+export default Mybutton;
